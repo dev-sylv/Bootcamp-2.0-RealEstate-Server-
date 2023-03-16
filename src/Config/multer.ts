ba6c26a45d6535d52c7e5c4b515056c4c56de7b4
nicsylvia <nicsylvia15f@gmail.com>
2023-03-16 12:31:19 -0700

    saved multer details

---
+++ src/Config/multer.ts
@@ -0,0 +1,35 @@
+import multer from "multer";
+
+import { Request } from "express";
+
+type DestinationCallBack = (error: Error | null, destination: string) => void
+
+type FileCallBack = (error: Error | null, filename: string) => void
+
+const Storage = multer.diskStorage({
+    destination: (
+        req: Request,
+        file: Express.Multer.File,
+        cb : DestinationCallBack
+    ) => {
+        cb(null, "Uploads")
+    },
+
+    filename: (
+        req: Request,
+        file: Express.Multer.File,
+        cb: FileCallBack
+    ) =>{
+        cb(null, file.originalname)
+    }
+});
+
+const AgentUploads = multer({
+    storage: Storage
+}).single("AgentImage");
+
+const HouseUploads = multer({
+    storage: Storage
+}).single("HouseImage");
+
+export {AgentUploads, HouseUploads};
