737474804bb8fcc8bd3d5dbccd67ea31bf3aefa9
nicsylvia <nicsylvia15f@gmail.com>
2023-03-16 12:42:59 -0700

    saved validator for validation

---
+++ src/Middlewares/Validation/validator.ts
@@ -0,0 +1,27 @@
+import { NextFunction } from "express";
+
+import Joi from "joi";
+
+import { AppError, HTTPCODES } from "../../Utils/AppError";
+
+export const validator = (
+    schemaName: Joi.ObjectSchema,
+    body: object,
+    next: NextFunction
+) =>{
+    const value = schemaName.validate(body, {
+        allowUnknown: true,
+        abortEarly: false,
+        stripUnknown: true,
+    });
+    try {
+        value.error ? next(
+            new AppError({
+                httpcode: HTTPCODES.UNPROCESSABLE_IDENTITY,
+                message: value.error.details[0].message,
+            })
+        ) : next()
+    } catch (error) {
+        console.log(error)
+    }
+};
