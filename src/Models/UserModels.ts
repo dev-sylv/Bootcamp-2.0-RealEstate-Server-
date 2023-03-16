baf63fdf2740a3c18b8e5819aede6bb12bd182dc
nicsylvia <nicsylvia15f@gmail.com>
2023-03-16 13:44:08 -0700

    saved user models

---
+++ src/Models/UserModels.ts
@@ -0,0 +1,61 @@
+import mongoose, { Schema, model } from "mongoose";
+
+import { IUSER } from "../AllInterfaces/UserInterfaces";
+
+import isEmail from "validator/lib/isEmail";
+
+const UserSchema: Schema<IUSER> = new Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: [isEmail, "Please enter a valid email"],
+    },
+    Image:{
+        type: String,
+    },
+    Bio:{
+        type: String,
+        required: [true, "Please enter a brief bio"]
+    },
+    phoneno:{
+        type: Number,
+        required: [true, "Please enter your Phone number"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your Password"]
+    },
+    confirmPassword: {
+        type: String,
+        required: [true, "Please confirm your password"]
+    },
+    role: {
+        type: String,
+        required: [true, "Please enter your role"],
+        message: "You must either be a User, Agent or Admin",
+        enum: ["User", "Admin", "Agent"],
+        default: "User"
+    },
+    houses:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Houses"
+        }
+    ]
+},
+{
+    timestamps: true
+});
+
+
+
+const UserModels = model<IUSER>("User", UserSchema);
+
+export default UserModels;
