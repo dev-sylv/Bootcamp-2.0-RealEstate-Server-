431b487b1f0140fd8dd9503b7a39e6428f1397e1
nicsylvia <nicsylvia15f@gmail.com>
2023-03-16 12:46:16 -0700

    saved user schema ans users validation

---
+++ src/Middlewares/Validation/UserValidation/UserValidation.ts
@@ -0,0 +1,7 @@
+import { RequestHandler } from "express";
+import { validator } from "../Validator";
+import { UserSchemaValidation } from "./UserSchemaValidation"
+
+export const UserRegisterValidation: RequestHandler = ( req, res, next ) => validator(UserSchemaValidation.Register, req.body, next);
+
+export const UserLoginValidation: RequestHandler = (req, res, next) => validator(UserSchemaValidation.Login, req.body, next)
