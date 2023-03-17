bb3a623ef0181307cf73e457a66cf82d5a229415
nicsylvia <nicsylvia15f@gmail.com>
2023-03-17 01:07:13 -0700

    saved all

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import { AppConfig } from "./app";
 import { DBCONNECTION } from "./Config/DB";
 import { EnvironmentVariables } from "./Config/environmentVariables";
@@ -8,6 +8,12 @@ const port = EnvironmentVariables.PORT;
 const app: Application = express();
 AppConfig(app)
 DBCONNECTION()
+
+app.get("/", (req: Request, res: Response) =>{
+    return res.status(200).json({
+        message: "API READY FOR CONSUMPTION"
+    })
+})
 
 app.listen(port, () =>{
     console.log("")
